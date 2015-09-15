16a3383306d36b8dec6cd7ef34af38530b48d266
jp <251099155@qq.com>
2015-09-15 17:22:42 +0800

    新增切换城市和手动更新天气功能

--- src/com/ssawweather/app/activity/WeatherActivity.java
+++ src/com/ssawweather/app/activity/WeatherActivity.java
@@ -6,16 +6,19 @@ import com.ssawweather.app.util.HttpUtil;
 import com.ssawweather.app.util.Utility;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements OnClickListener {
 	
 	private LinearLayout weatherInfoLayout;
 	
@@ -42,7 +45,18 @@ public class WeatherActivity extends Activity {
 	/*用于显示当前日期*/
 	
 	private TextView currentDateText;
+	
+	/*切换城市按钮
+	*/
 
+	private Button switchCity;
+	
+	/*更换天气按钮*/
+	
+	private Button refreshWeather;
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -73,7 +87,48 @@ public class WeatherActivity extends Activity {
 			
 			showWeather();
 		}
+		
+		
+		switchCity=(Button) findViewById(R.id.switch_city);
+		
+		refreshWeather=(Button) findViewById(R.id.refresh_weather);
+		
+		switchCity.setOnClickListener(this);
+		
+		refreshWeather.setOnClickListener(this);
+		
 	}
+	
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.switch_city:
+			Intent intent =new Intent(this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+
+		case R.id.refresh_weather:
+			publishText.setText("同步中。。。");
+			SharedPreferences prefs=PreferenceManager
+					.getDefaultSharedPreferences(this);
+			String weatherCode=prefs.getString("weather_code", "");
+			if (!TextUtils.isEmpty(weatherCode)) {
+				queryWeatherCode(weatherCode);
+				
+			}
+			break;
+	
+		default:
+			break;
+		}
+	}
+
+	
+	
 
 	/*
 	查询县级代号所对应的天气代号*/
@@ -165,6 +220,7 @@ public class WeatherActivity extends Activity {
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
 	}
+
 
 
 	
